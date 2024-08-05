6623b767ae2db1eb7bd5b5bce4da6d435a23dc74
Bilal-EZ-ZAIM <bilalzaim499@gmail.com>
2024-08-05 14:33:10 +0100

    Implement category management with endpoints for creation, retrieval, update, and deletion

--- server/router/categoryRouter.js
+++ server/router/categoryRouter.js
@@ -9,11 +9,12 @@ const {
   ValiditeCreat,
   ValiditeUpdate,
 } = require("../util/validateur/ValiditCategory");
+const validateObjectId = require("../util/validateObjectId");
 const router = express.router();
 
 router.get("/getAll", getAllCategory);
 router.post("/add", ValiditeCreat, addCategory);
-router.put("/update/:id", ValiditeUpdate, updateCategoryById);
-router.delete("/delete/:id", deleteCategoryById);
+router.put("/update/:id", validateObjectId, ValiditeUpdate, updateCategoryById);
+router.delete("/delete/:id", validateObjectId, deleteCategoryById);
 
 module.exports = router;
